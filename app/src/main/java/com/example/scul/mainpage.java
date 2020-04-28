873aeb420efa3fed84ac84f0edbe52449458f218
swopnil <parajuliswopnil717@gmail.com>
2020-04-28 11:59:07 +0545

    backend for navigation bar

--- app/src/main/java/com/example/scul/mainpage.java
+++ app/src/main/java/com/example/scul/mainpage.java
@@ -1,8 +1,13 @@
 package com.example.scul;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class mainpage extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class mainpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.notification:
+                        startActivity(new Intent(getApplicationContext(), Notification.class));
+                        overridePendingTransition(0,0);
+                    case R.id.home:
+
+                    case R.id.classes:
+                        startActivity(new Intent(getApplicationContext(), Classes.class));
+                        overridePendingTransition(0,0);
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext(), Account.class));
+                        overridePendingTransition(0,0);
+                }
+
+            }
+        });
     }
 }
